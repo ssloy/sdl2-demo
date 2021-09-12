2479e6db0c97858cc3c0e4df7393cfbe11498cc7
Dmitry Sokolov <ha@haqr.eu>
2021-09-12 21:59:55 +0200

    windows SDL issues

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#define SDL_MAIN_HANDLED
 #include <SDL.h>
 
 int main() {
+    SDL_SetMainReady();
     if (SDL_Init(SDL_INIT_VIDEO)) {
         std::cerr << "Failed to initialize SDL: " << SDL_GetError() << std::endl;
         return -1;
